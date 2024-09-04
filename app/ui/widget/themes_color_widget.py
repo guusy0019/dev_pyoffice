71752297587e6219331b0c26e5f1e693dd56b56e
guusy0019 <mattyan0915@gmail.com>
2024-09-04 22:24:19 +0900

    テーマ変更はうまくいかないが、とりあえずダウンロードしたので保存だけしておく

--- app/ui/widget/themes_color_widget.py
+++ app/ui/widget/themes_color_widget.py
@@ -7,11 +7,18 @@ class ThemesColorWidget(customtkinter.CTkOptionMenu):
             master,
             values=["blue", "green", "dark-blue"],
             command=self.change_themes_color_event,
-            **kwargs
+            **kwargs,
         )
 
     def change_themes_color_event(self, themes_color):
-        customtkinter.set_default_color_theme(themes_color)
+        try:
+            customtkinter.set_default_color_theme(themes_color)
+            print(f"Themes color changed to {themes_color}")
+        except Exception as e:
+            print(f"Error: {e}")
 
 
 # なぜうまくいかん？
+# customtkinter.navigation_frameのインスタンスでは変更不可のため、最初にプルダウンからテーマを選ばせる
+# その後、テーマの文字列を保存してインスタンスを再起動するのが必要ぽいね
+# まあ、後回しかな、ただテーマだけは保存しておく
